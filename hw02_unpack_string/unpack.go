d1379c24c50c4ea41a52bbbdeb250b9e2667e530
Andrey Zherikhov <aszherikhov@tsum.ru>
2021-06-18 17:05:44 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,57 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var sb strings.Builder
+
+	runeInput := []rune(input)
+	escaped := false
+
+	for i, rune := range runeInput {
+		isDigitNext := (len(runeInput) > i+1) && unicode.IsDigit(runeInput[i+1])
+		isDigit := unicode.IsDigit(rune)
+		isSlash := rune == 92 // '\' ~ 92
+
+		if isDigit && !escaped {
+			if i == 0 || isDigitNext {
+				return "", ErrInvalidString
+			}
+			continue
+		}
+
+		if isSlash && !escaped {
+			escaped = true
+			continue
+		}
+
+		if escaped && !isDigit && !isSlash {
+			return "", ErrInvalidString
+		}
+
+		if isDigit || isSlash || !escaped {
+			if isDigitNext {
+				repeatTimes, _ := strconv.Atoi(string(runeInput[i+1]))
+				repeatedRune := strings.Repeat(string(rune), repeatTimes)
+
+				sb.WriteString(repeatedRune)
+			} else {
+				sb.WriteRune(rune)
+			}
+		}
+
+		escaped = false
+	}
+
+	if escaped {
+		return "", ErrInvalidString
+	}
+
+	return sb.String(), nil
 }
