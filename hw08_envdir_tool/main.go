0151a55ba26250e41a723c5eefaa5de4b2a83e24
Andrey Zherikhov <aszherikhov@tsum.ru>
2021-09-25 17:42:07 +0300

    XHW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	envPath := os.Args[1]
+	cmd := os.Args[2:]
+
+	env, err := ReadDir(envPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	RunCmd(cmd, env)
 }
